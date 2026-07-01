c664507cf322c2f9bf666c15af90302ced5a06c3
Adenuga Willy <adenugawilly431@gmail.com>
2026-07-01 16:06:33 +0100

    fix:asynchronous-operation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from 'src/dtos/auth-dto/signup.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Users')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
+  @ApiOperation({ summary: 'Create a new user account' })
   @Post('register')
   async createUser(@Body() dto: SignUpDto) {
-    return this.authService.createUser(dto);
+    return await this.authService.createUser(dto);
   }
 }
